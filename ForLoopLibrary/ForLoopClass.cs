2d51bd21922ea6baaddb167ec51f8672d7efe47c
Jesualdo Pinheiro Santos Quotation Factory <jesualdo_pinheiro@outlook.com>
2025-01-06 23:01:52 -0300

    For Loop exercises added

--- ForLoopLibrary/ForLoopClass.cs
+++ ForLoopLibrary/ForLoopClass.cs
@@ -1,4 +1,10 @@
-﻿namespace ForLoopLibrary
+﻿
+
+using System.Linq;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ForLoopLibrary
 {
     public class ForLoopClass
     {
@@ -21,6 +27,155 @@
             {
                 result += i;
             }
+            return result;
+        }
+
+        public string AverageAndSum(int start, int step, int length)
+        {
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                int value = start + i * step;
+                sum += value;
+            }
+            double average = (double)sum / length;
+
+            return $"The sum is {sum} and the average is {average}";
+        }
+
+        public double[] CubeOfNaturalNumbers(int input)
+        {
+            var result = new double[input];
+
+            for (int i = 1; i <= input; i++)
+            {
+                result[i - 1] = Math.Pow(i, 3);
+            }
+
+            return result;
+        }
+
+        public int[] ArrayMultiplicatorSequence(int input, int limit)
+        {
+            var result = new int[limit];
+
+            for (int i = 1; i <= limit; i++)
+            {
+                result[i - 1] = i * input;
+            }
+            return result;
+        }
+
+        public string[][] MultiplicationTables(int input)
+        {
+            string[][] table = new string[input][];
+
+            for (int i = 1; i <= input; i++)
+            {
+                table[i - 1] = new string[10];
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    table[i - 1][j - 1] = $"{i} x {j} = {i * j}";
+                }
+            }
+
+            return table;
+        }
+
+        public string OddNumbersAndSum(int input)
+        {
+            var result = new int[input];
+            int num = 1;
+            for (int j = 0; j < input; j++)
+            {
+                while (num % 2 == 0)
+                {
+                    num++;
+                }
+                result[j] = num;
+                num++;
+            }
+            return $"The odd list is [{string.Join(",", result)}] and their sum is {result.Sum()}";
+        }
+
+        public string AsteriscTriangle(int input)
+        {
+            var result = new string[input];
+
+            for (int i = 1; i <= input; i++)
+            {
+                result[i - 1] = string.Concat(Enumerable.Repeat("*", i));
+            }
+            return string.Join("\n", result);
+        }
+
+        public string TriangleWithSequence(int input)
+        {
+            string result = "";
+
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    result += j.ToString();
+                }
+                result += "\n";
+            }
+            return result.Trim();
+        }
+
+        public string TriangleWithRepetitions(int input)
+        {
+            string result = "";
+            for (int i = 1; i <= input; i++)
+            {
+                result += string.Concat(Enumerable.Repeat(i.ToString(), i));
+                result += "\n";
+            }
+            return result.Trim();
+        }
+
+        public string TriangleWithIncreasingNumber(int input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            int num = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    result.Append(num).Append(" ");
+                    num++;
+                }
+                result.Length--;
+                result.Append("\n");
+            }
+            return result.ToString().Trim();
+        }
+
+        public string PiramidOfNumbers(int input)
+        {
+            string result = "";
+            int spaces = input - 1;
+            int num = 1;
+
+            for (int i = 1; i <= input; i++)
+            {
+                result += string.Concat(Enumerable.Repeat(" ", spaces));
+
+                for (int j = 0; j < i; j++)
+                {
+                    result += num + " ";
+                    num++;
+                }
+
+                result += string.Concat(Enumerable.Repeat(" ", spaces));
+                result = result.Substring(0, result.Length - 1);
+                spaces--;
+                result += "\n";
+            }
+            result = result.Substring(0, result.Length - 1);
 
             return result;
         }
--- ForLoopTests/ForLoopTestClass.cs
+++ ForLoopTests/ForLoopTestClass.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using ForLoopLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ForLoopTests
 {
@@ -15,18 +16,21 @@ namespace ForLoopTests
         }
 
         [TestMethod]
-        [DataRow(10)]
-        public void ShouldReturnNaturalNumbersUntilInput(int input)
+        [DataRow(10, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+        [DataRow(8, new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void ShouldReturnNaturalNumbersUntilInput(int input, int[] expectedResult)
         {
             //Act
             int[] result = _exercises.IntCounter(input);
 
             //Assert
-            result.Should().Equal(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            result.Should().Equal(expectedResult);
         }
 
         [TestMethod]
         [DataRow(10, 55)]
+        [DataRow(5, 15)]
+        [DataRow(7, 28)]
         public void ShouldReturnTheSumOfTheNumbersOfArray(int input, int expectedResult)
         {
             //Act
@@ -37,10 +41,126 @@ namespace ForLoopTests
         }
 
         [TestMethod]
-        [DataRow()]
-        public void Should()
+        [DataRow(2, 2, 10, 110, 11)]
+        [DataRow(3, 3, 20, 630, 31.5)]
+        public void Should(int start, int step, int length, int sum, double average)
         {
+            //Act
+            string result = _exercises.AverageAndSum(start, step, length);
 
+            //Assert
+            result.Should().Be($"The sum is {sum} and the average is {average}");
+        }
+
+        [TestMethod]
+        [DataRow(5, new[] { 1.0, 8.0, 27.0, 64.0, 125.0 })]
+        [DataRow(7, new[] { 1.0, 8.0, 27.0, 64.0, 125.0, 216.0, 343.0 })]
+        public void ShouldReturnTheCubeOfAllNaturalNumbersUpToInput(int input, double[] expectedResult)
+        {
+            //Act
+            double[] result = _exercises.CubeOfNaturalNumbers(input);
+
+            //Assert
+            result.Should().Equal(expectedResult);
+        }
+
+        [TestMethod]
+        [DataRow(15, 10, new[] { 15, 30, 45, 60, 75, 90, 105, 120, 135, 150 })]
+        [DataRow(8, 10, new[] { 8, 16, 24, 32, 40, 48, 56, 64, 72, 80 })]
+        public void ShouldMultiplicateInputThroughOneToN(int input, int limit, int[] expectedResult)
+        {
+            //Act
+            int[] result = _exercises.ArrayMultiplicatorSequence(input, limit);
+
+            //Assert
+            result.Should().Equal(expectedResult);
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        public void ShouldCreateAMultiplicationTable(int input)
+        {
+            //Act
+            string[][] result = _exercises.MultiplicationTables(input);
+
+            //Assert
+            result.Should().BeEquivalentTo(Enumerable.Range(1, input)
+                .Select(i => Enumerable.Range(1, 10)
+                .Select(j => $"{i} x {j} = {i * j}").ToArray()));
+        }
+
+        [TestMethod]
+        [DataRow(5, new[] { 1, 3, 5, 7, 9 }, 25)]
+        [DataRow(10, new[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 }, 100)]
+        public void ShouldReturnNOddNumbersAndTheirSum(int input, int[] oddNumbers, int sum)
+        {
+            //Act
+            string result = _exercises.OddNumbersAndSum(input);
+
+            //Assert
+            result.Should().Be($"The odd list is [{string.Join(",", oddNumbers)}] and their sum is {sum}");
+        }
+
+        [TestMethod]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        public void ShouldReturnAnAsteriscTriangle(int input)
+        {
+            //Act
+            string result = _exercises.AsteriscTriangle(input);
+
+            //Assert
+            result.Should().Be(string.Join("\n", Enumerable.Range(1, input)
+                .Select(i => new string('*', i))));
+        }
+
+        [TestMethod]
+        [DataRow(4)]
+        [DataRow(6)]
+        public void ShouldCreateATriangleWithASequence(int input)
+        {
+            //Act
+            string result = _exercises.TriangleWithSequence(input);
+
+            //Assert
+            result.Should().Be(string.Join("\n", Enumerable.Range(1, input)
+                .Select(i => string.Concat(Enumerable.Range(1, i)))));
+        }
+
+        [TestMethod]
+        [DataRow(4)]
+        [DataRow(5)]
+        public void ShouldCreateATriangleWithRepetitions(int input)
+        {
+            //Act
+            string result = _exercises.TriangleWithRepetitions(input);
+
+            //Assert
+            result.Should().Be(string.Join("\n", Enumerable.Range(1, input)
+                .Select(i => string.Concat(Enumerable.Repeat(i.ToString(), i)))));
+        }
+
+        [TestMethod]
+        [DataRow(4)]
+        public void ShouldCreateATriangleWithTheNumberIncreasedBy1(int input)
+        {
+            //Act
+            string result = _exercises.TriangleWithIncreasingNumber(input);
+
+            //Assert
+            result.Should().Be($"1\n2 3\n4 5 6\n7 8 9 10");
+        }
+
+        [TestMethod]
+        [DataRow(4)]
+        public void ShouldCreateAPiramidMadeOfNumbers(int input)
+        {
+            //Act
+            string result = _exercises.PiramidOfNumbers(input);
+
+            //Assert
+            result.Should().Be("   1   \n  2 3  \n 4 5 6 \n7 8 9 10");
         }
     }
 }
