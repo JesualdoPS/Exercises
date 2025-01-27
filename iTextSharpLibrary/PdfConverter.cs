aeb71aaaaab1ee835e3cb11c747e0cf2dd9741d5
Jesualdo Pinheiro Santos Quotation Factory <jesualdo_pinheiro@outlook.com>
2025-01-27 15:15:12 -0300

    Reducing and turning PDF to images

---
+++ iTextSharpLibrary/PdfConverter.cs
@@ -0,0 +1,83 @@
+﻿using SautinSoft;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfConverterLibrary
+{
+    public class PdfConverter
+    {
+        public void PDFtoImage(string pdfPath, int pages, string imagePath, int dpi = 400)
+        {
+            try
+            {
+                byte[] pdfBytes = ExtractPages(pdfPath, pages);
+                var newPdf = new MemoryStream(pdfBytes);
+                string tempPdfPath = Path.GetTempFileName() + ".pdf";
+
+                using (var fs = new FileStream(tempPdfPath, FileMode.Create))
+                {
+                    newPdf.WriteTo(fs);
+                }
+
+                var pdf = new PdfFocus();
+                pdf.OpenPdf(tempPdfPath);
+                if (pdf.PageCount > 0)
+                {
+                    pdf.ImageOptions.Dpi = dpi;
+                    pdf.ToImages(imagePath, $"Output.");
+                }
+                pdf.ClosePdf();
+                File.Delete(tempPdfPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private byte[] ExtractPages(string pdfPath, int pages)
+        {
+            try
+            {
+                var pdfReader = new PdfReader(pdfPath);
+                var doc = new Document();
+                var memoryStream = new MemoryStream();
+                var copy = new PdfCopy(doc, memoryStream);
+                doc.Open();
+
+                for (int i = 1; i <= pages; i++)
+                {
+                    if (i > 0 && i <= pdfReader.NumberOfPages)
+                    {
+                        copy.AddPage(copy.GetImportedPage(pdfReader, i));
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Page {i} does not exists");
+                    }
+                }
+
+                doc.Close();
+                pdfReader.Close();
+                return memoryStream.ToArray();
+            }
+            catch (ArgumentException ex)
+            {
+                throw new Exception($"Error extracting pages: {ex.Message}");
+            }
+            catch (IOException ex)
+            {
+                throw new Exception($"Error reading or writing file: {ex.Message}");
+            }
+            catch (OutOfMemoryException)
+            {
+                throw new Exception($"Error: out of memory");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error extracting pages: {ex.Message}");
+            }
+        }
+    }
+}
+
---
+++ iTextSharpProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using PdfConverterLibrary;
+
+namespace PdfConverterProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new PdfConverter();
+            string pdfFilePath = "E:\\Downloads\\PotatoBeetle.pdf";
+            string imagePath = "E:\\Downloads\\ConvertedImages";
+            int pages = 2;
+            converter.PDFtoImage(pdfFilePath, pages, imagePath);
+            Console.WriteLine("Success");
+        }
+    }
+}
